b787ac1816e0f5f3a3f42e8ca8cddc5c252dfe07
Andrey A. Konovalov <drvtiny@gmail.com>
2018-07-05 21:08:47 +0300

    Set %SIG as desribed in README

--- lib/Clean/On/Exit.pm
+++ lib/Clean/On/Exit.pm
@@ -3,22 +3,40 @@ use Exporter qw(import);
 use Scalar::Util qw(refaddr);
 use Carp qw(confess);
 use File::Spec;
+use constant { TRUE => 1, FALSE => undef };
 
 our @EXPORT = qw/clean_on_exit wipe_on_exit/;
 
-my %toClean;
+my (%toClean, $flNotEmpty);
 sub clean_on_exit {
     return unless $#_ >= 0 and defined $_[0];
-    my $r = ref $_[0];
-    unless ( $r ) {
-        my $pth2file = $#_ ? File::Spec->catfile(@_) : $_[0];
-        return $pth2file if $toClean{'files'}{'paths'}{$pth2file};
-        $toClean{'files'}{'paths'}{$pth2file} = ($toClean{'files'}{'index'}++ // 0);
-    } elsif ($r eq 'CODE') {
-        $toClean{'code'}{'pocs'}{refaddr $_[0]} //= [$toClean{'code'}{'index'}++ // 0, shift, \@_]
-    } else {
-        confess "Cant clean anything using reference of type <<$r>>, sorry :)"
+    my $ref0 = ref $_[0];
+    my $retv = do {
+        unless ( $ref0 ) {
+            my $pth2file = $#_ ? File::Spec->catfile(@_) : $_[0];
+            $toClean{'files'}{'paths'}{$pth2file} = $toClean{'files'}{'index'}++ // 0
+                unless $flNotEmpty and defined($toClean{'files'}{'paths'}{$pth2file});
+            $pth2file
+        } elsif ($ref0 eq 'CODE') {
+            $toClean{'code'}{'pocs'}{refaddr $_[0]} //= [$toClean{'code'}{'index'}++ // 0, my $poc = shift, my $args = \@_];
+            sub { $poc->(@{$args}) };
+        } else {
+            confess "Cant clean anything using reference of type <<$r>>, sorry :)"
+        }
+    };
+    
+    unless ($flNotEmpty) {
+        $flNotEmpty = TRUE;
+        for (qw/INT TERM/) {
+            my $saveSIGHndl = $SIG{$_} // sub { exit 1 };
+            $SIG{$_} = sub {
+                make_mrproper;
+                $saveSIGHndl->();
+            }
+        }
     }
+    
+    $retv
 }
 
 {
@@ -37,14 +55,6 @@ sub make_mrproper {
         }
     }
     %toClean = ()
-}
-
-for (qw/INT TERM/) {
-    my $saveSIGHndl = $SIG{$_} // sub { exit 1 };
-    $SIG{$_} = sub {
-        make_mrproper;
-        $saveSIGHndl->();
-    }
 }
 
 END { make_mrproper }
